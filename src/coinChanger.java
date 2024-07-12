f98a19d3cf6eb64b00ca4b7122397e6f1e925e7e
Alex Root-Roatch <arootroatch@gmail.com>
2024-07-12 11:38:04 -0500

    one class

--- src/coinChanger.java
+++ src/coinChanger.java
@@ -3,24 +3,17 @@ import java.util.HashMap;
 public class coinChanger {
 
     public static HashMap<String, Integer> makeChange(Double amount) {
-        HashMap<String, Integer> coins = new HashMap<String, Integer>();
+        HashMap<String, Integer>coins = new HashMap<String, Integer>();
+        String[] coinNames = {"quarters", "dimes", "nickels", "pennies"};
+        int[] coinValues = {25, 10, 5, 1};
         int n = (int) (Math.round(amount * 100));
 
-        return calculateCoins(n, coins);
-    }
-
-    private static HashMap<String, Integer> calculateCoins(Integer n, HashMap<String, Integer> coins) {
-        int[] coinValues = {25, 10, 5, 1};
-        String[] coinNames = {"quarters", "dimes", "nickels", "pennies"};
-
-        for (int i = 0; i < coinValues.length; i++) {
-            if (n >= coinValues[i]) {
-                int numCoins = (n / coinValues[i]);
-                coins.put(coinNames[i], numCoins);
+        for (int i = 0; i < coinNames.length; i++){
+            if (n >= coinValues[i]){
+                coins.put(coinNames[i], (n / coinValues[i]));
                 n %= coinValues[i];
             }
         }
-
         return coins;
     }
 }
