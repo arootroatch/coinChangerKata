380243a1404e173bb552084e9d2c36789bcb9921
Alex Root-Roatch <arootroatch@gmail.com>
2024-07-24 13:33:37 -0500

    updated approach, added null checks to tests

--- src/CoinChangerTest.java
+++ src/CoinChangerTest.java
@@ -14,7 +14,7 @@ class CoinChangerTest {
     }
 
     @Test
-    void zeroCoins() {
+    void zeroCoins(){
         coins = coinChanger.makeChange(0);
         assertEquals(0, coins.size());
     }
@@ -32,15 +32,10 @@ class CoinChangerTest {
     }
 
     @Test
-    void threePennies(){
-        coins = coinChanger.makeChange(3);
-        assertEquals(3, coins.get("pennies"));
-    }
-
-    @Test
     void oneNickel(){
         coins = coinChanger.makeChange(5);
         assertEquals(1, coins.get("nickels"));
+        assertEquals(null, coins.get("pennies"));
     }
 
     @Test
@@ -61,75 +56,42 @@ class CoinChangerTest {
     void oneDime(){
         coins = coinChanger.makeChange(10);
         assertEquals(1, coins.get("dimes"));
-    }
-
-    @Test
-    void oneDimeOneNickel(){
-        coins = coinChanger.makeChange(15);
-        assertEquals(1, coins.get("nickels"));
-        assertEquals(1, coins.get("dimes"));
+        assertEquals(null, coins.get("pennies"));
+        assertEquals(null, coins.get("nickels"));
     }
 
     @Test
     void oneDimeOneNickelOnePenny(){
         coins = coinChanger.makeChange(16);
-        assertEquals(1, coins.get("nickels"));
-        assertEquals(1, coins.get("pennies"));
         assertEquals(1, coins.get("dimes"));
-    }
-
-    @Test
-    void twoDimes(){
-        coins = coinChanger.makeChange(20);
-        assertEquals(2, coins.get("dimes"));
+        assertEquals(1, coins.get("pennies"));
+        assertEquals(1, coins.get("nickels"));
     }
 
     @Test
     void oneQuarter(){
         coins = coinChanger.makeChange(25);
         assertEquals(1, coins.get("quarters"));
-    }
-
-    @Test
-    void oneQuarterOneNickel(){
-        coins = coinChanger.makeChange(30);
-        assertEquals(1, coins.get("nickels"));
-        assertEquals(1, coins.get("quarters"));
-    }
-
-    @Test
-    void oneQuarterOneDime(){
-        coins = coinChanger.makeChange(35);
-        assertEquals(1, coins.get("quarters"));
-        assertEquals(1, coins.get("dimes"));
-    }
-
-    @Test
-    void oneQuarterOneDimeOnePenny(){
-        coins = coinChanger.makeChange(36);
-        assertEquals(1, coins.get("quarters"));
-        assertEquals(1, coins.get("pennies"));
-        assertEquals(1, coins.get("dimes"));
-    }
-
-    @Test
-    void twoQuarters(){
-        coins = coinChanger.makeChange(50);
-        assertEquals(2, coins.get("quarters"));
-    }
-
-    @Test
-    void fourQuarters(){
-        coins = coinChanger.makeChange(100);
-        assertEquals(4, coins.get("quarters"));
+        assertEquals(null, coins.get("pennies"));
+        assertEquals(null, coins.get("nickels"));
+        assertEquals(null, coins.get("dimes"));
     }
 
     @Test
     void fourQuartersOneDimeOneNickelOnePenny(){
         coins = coinChanger.makeChange(116);
-        assertEquals(1, coins.get("nickels"));
-        assertEquals(1, coins.get("pennies"));
-        assertEquals(1, coins.get("dimes"));
         assertEquals(4, coins.get("quarters"));
+        assertEquals(1, coins.get("pennies"));
+        assertEquals(1, coins.get("nickels"));
+        assertEquals(1, coins.get("dimes"));
+    }
+
+    @Test
+    void fourQuartersOneDimeOneNickelFourPennies(){
+        coins = coinChanger.makeChange(119);
+        assertEquals(4, coins.get("quarters"));
+        assertEquals(4, coins.get("pennies"));
+        assertEquals(1, coins.get("nickels"));
+        assertEquals(1, coins.get("dimes"));
     }
 }
--- src/coinChanger.java
+++ src/coinChanger.java
@@ -2,15 +2,15 @@ import java.util.HashMap;
 
 public class coinChanger {
 
-    public static HashMap<String, Integer> makeChange(Integer amount) {
+    public static HashMap<String, Integer> makeChange(Integer n) {
         HashMap<String, Integer>coins = new HashMap<String, Integer>();
         String[] coinNames = {"quarters", "dimes", "nickels", "pennies"};
         int[] coinValues = {25, 10, 5, 1};
 
         for (int i = 0; i < coinNames.length; i++){
-            if (amount >= coinValues[i]){
-                coins.put(coinNames[i], (amount / coinValues[i]));
-                amount %= coinValues[i];
+            if (n >= coinValues[i]){
+                coins.put(coinNames[i], (n / coinValues[i]));
+                n %= coinValues[i];
             }
         }
         return coins;
